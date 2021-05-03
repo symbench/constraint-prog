7e9b3c7e3c3cdbed38e77df37099a705d4f042ec
Zsolt Vizi <vizizsolt89@gmail.com>
2021-05-03 15:38:34 +0200

    Add flag for benchmark calculation

--- constraint_prog/gradient_descent.py
+++ constraint_prog/gradient_descent.py
@@ -68,6 +68,7 @@ def gradient_descent(f: Callable, in_data: torch.Tensor, it: int,
     [*, output_size].
     The returned tensor is of shape [*, input_size].
     """
+    is_benchmark_calculated = False
     # Apply standard scaling for the input data
     scaler = TorchStandardScaler()
     scaler.fit(x=in_data)
@@ -79,14 +80,15 @@ def gradient_descent(f: Callable, in_data: torch.Tensor, it: int,
     )
     inp_data.requires_grad = True
 
-    # Benchmark: create data and list of optimizers
     inp_data_bench = []
     optim_list = []
-    for x in inp_data:
-        y = x.clone().detach()
-        y.requires_grad = True
-        inp_data_bench.append(y)
-        optim_list.append(torch.optim.SGD(params=[y], lr=lrate))
+    if is_benchmark_calculated:
+        # Benchmark: create data and list of optimizers
+        for x in inp_data:
+            y = x.clone().detach()
+            y.requires_grad = True
+            inp_data_bench.append(y)
+            optim_list.append(torch.optim.SGD(params=[y], lr=lrate))
 
     for _ in range(it):
         # Proposed solution
@@ -106,17 +108,18 @@ def gradient_descent(f: Callable, in_data: torch.Tensor, it: int,
         # 5. Reset gradient
         inp_data.grad.zero_()
 
-        # Benchmark
-        for data, optim in zip(inp_data_bench, optim_list):
-            # 1. Rescale data (vector) for evaluating f
-            data_rescaled = scaler.rescale(x=data.view((1, -1)))
-            # 2. Compute squared error from zero
-            val_bench = (f(data_rescaled).pow(2.0).sum(dim=-1)).pow(0.5)
-            # 3. Reset gradient
-            optim.zero_grad()
-            # 4. Compute gradients
-            val_bench.backward()
-            # 5. Apply one step of gradient descent method
-            optim.step()
+        if is_benchmark_calculated:
+            # Benchmark
+            for data, optim in zip(inp_data_bench, optim_list):
+                # 1. Rescale data (vector) for evaluating f
+                data_rescaled = scaler.rescale(x=data.view((1, -1)))
+                # 2. Compute squared error from zero
+                val_bench = (f(data_rescaled).pow(2.0).sum(dim=-1)).pow(0.5)
+                # 3. Reset gradient
+                optim.zero_grad()
+                # 4. Compute gradients
+                val_bench.backward()
+                # 5. Apply one step of gradient descent method
+                optim.step()
 
     return scaler.rescale(x=inp_data)
