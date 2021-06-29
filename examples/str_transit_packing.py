708a6f21d991ebdbde720aae7d6a8dff9c165e1d
Miklos Maroti <mmaroti@gmail.com>
2021-06-29 17:46:07 +0200

    somewhat working version

--- examples/str_transit_packing.py
+++ examples/str_transit_packing.py
@@ -14,9 +14,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from re import fullmatch
+import math
 import sympy
-from sympy import pi, cos, sin, tan, sqrt, log, exp, ceiling
+from sympy import pi, cos, sin, tan, sqrt, log, exp, ceiling, atan
+
+from constraint_prog.point_cloud import PointCloud, PointFunc
+
+# lets use floating points
+pi = 3.14159265359
 
 WATER_DENSITY_AT_SEA_LEVEL = 1027.0  # kg/m^3
 OIL_DENSITY = 837.0  # kg/m^3
@@ -509,32 +514,32 @@ center_of_buoyancy = {
               movable_roll_z_center * movable_roll_displacement) / vehicle_total_displacement_bladder_empty}
 }
 
-pitch_angle_minimum = sympy.atan(
+pitch_angle_minimum = atan(
     (center_of_gravity['bladder_empty_pitch_forward_roll_center']['x'] -
      center_of_buoyancy['bladder_empty_pitch_forward_roll_center']['x']) /
     (center_of_buoyancy['bladder_empty_pitch_forward_roll_center']['z'] -
      center_of_gravity['bladder_empty_pitch_forward_roll_center']['z'])) * 180.0 / pi
-pitch_angle_neutral = sympy.atan(
+pitch_angle_neutral = atan(
     (center_of_gravity['bladder_half_pitch_middle_roll_center']['x'] -
      center_of_buoyancy['bladder_half_pitch_middle_roll_center']['x']) /
     (center_of_buoyancy['bladder_half_pitch_middle_roll_center']['z'] -
      center_of_gravity['bladder_half_pitch_middle_roll_center']['z'])) * 180.0 / pi
-pitch_angle_maximum = sympy.atan(
+pitch_angle_maximum = atan(
     (center_of_gravity['bladder_full_pitch_aft_roll_center']['x'] -
      center_of_buoyancy['bladder_full_pitch_aft_roll_center']['x']) /
     (center_of_buoyancy['bladder_full_pitch_aft_roll_center']['z'] -
      center_of_gravity['bladder_full_pitch_aft_roll_center']['z'])) * 180.0 / pi
-roll_angle_minimum = sympy.atan(
+roll_angle_minimum = atan(
     (center_of_gravity['bladder_half_pitch_middle_roll_port']['y'] -
      center_of_buoyancy['bladder_half_pitch_middle_roll_port']['y']) /
     (center_of_buoyancy['bladder_half_pitch_middle_roll_port']['z'] -
      center_of_gravity['bladder_half_pitch_middle_roll_port']['z'])) * 180.0 / pi
-roll_angle_neutral = sympy.atan(
+roll_angle_neutral = atan(
     (center_of_gravity['bladder_half_pitch_middle_roll_center']['y'] -
      center_of_buoyancy['bladder_half_pitch_middle_roll_center']['y']) /
     (center_of_buoyancy['bladder_half_pitch_middle_roll_center']['z'] -
      center_of_gravity['bladder_half_pitch_middle_roll_center']['z'])) * 180.0 / pi
-roll_angle_maximum = sympy.atan(
+roll_angle_maximum = atan(
     (center_of_gravity['bladder_half_pitch_middle_roll_starboard']['y'] -
      center_of_buoyancy['bladder_half_pitch_middle_roll_starboard']['y']) /
     (center_of_buoyancy['bladder_half_pitch_middle_roll_starboard']['z'] -
@@ -551,8 +556,207 @@ roll_angle_minimum_equation = roll_angle_minimum <= -20
 roll_angle_maximum_equation = roll_angle_maximum >= 20
 roll_angle_neutral_equation = sympy.Abs(roll_angle_neutral) <= allowable_roll_error_at_neutral
 
-
 # Assumptions:
 #   movable_roll_displacement = volume of entire possible location for the mass
 #   movable_pitch_displacement = volume of entire possible location for the mass
 #   z-placements of all components are 0 (not true in the spreadsheet for Battery Pack 1)
+
+
+def get_weighted_wet_masses(bladder: str, pitch: str, roll: str) \
+        -> (sympy.Expr, sympy.Expr, sympy.Expr, sympy.Expr):
+    """
+    Returns the total wet mass and the weighted sum of x, y and z 
+    coordinates of components. To get the center of gravity of the wet
+    weight, divide x, y and z with the total wet mass.
+    """
+    total_mass = 0
+    total_x_sum = 0
+    total_y_sum = 0
+    total_z_sum = 0
+
+    total_mass += foam1_wet_mass
+    total_x_sum += foam1_wet_mass * foam1_x_center
+
+    total_mass += vessel1_dry_mass
+    total_x_sum += vessel1_wet_mass * vessel1_x_center
+
+    total_mass += battery1_wet_mass
+    total_x_sum += battery1_wet_mass * battery1_x_center
+
+    total_mass += vessel2_dry_mass
+    total_x_sum += vessel2_wet_mass * vessel2_x_center
+
+    if bladder == "empty":
+        total_mass += reservoir1_wet_mass_full
+        total_x_sum += reservoir1_wet_mass_full * reservoir1_x_center
+    elif bladder == "half":
+        total_mass += reservoir1_wet_mass_half
+        total_x_sum += reservoir1_wet_mass_half * reservoir1_x_center
+    else:
+        assert bladder == "full"
+        total_mass += reservoir1_wet_mass_empty
+        total_x_sum += reservoir1_wet_mass_empty * reservoir1_x_center
+
+    if bladder == "empty":
+        total_mass += bladder1_wet_mass_empty
+        total_x_sum += bladder1_wet_mass_empty * bladder1_x_center
+    elif bladder == "half":
+        total_mass += bladder1_wet_mass_half
+        total_x_sum += bladder1_wet_mass_half * bladder1_x_center
+    elif bladder == "full":
+        total_mass += bladder1_wet_mass_full
+        total_x_sum += bladder1_wet_mass_full * bladder1_x_center
+
+    total_mass += foam2_wet_mass
+    total_x_sum += foam2_wet_mass * foam2_x_center
+
+    total_mass += wing_wet_mass
+    total_x_sum += wing_wet_mass * wing_x_center
+    total_z_sum += wing_wet_mass * wing_z_center
+
+    total_mass += movable_roll_wet_mass
+    total_x_sum += movable_roll_wet_mass * movable_roll_x_center
+    total_z_sum += movable_roll_wet_mass * movable_roll_z_center
+    if roll == "starboard":
+        total_y_sum += movable_roll_wet_mass * movable_roll_y_center_stb
+    elif roll == "center":
+        total_y_sum += movable_roll_wet_mass * movable_roll_y_center_mid
+    else:
+        assert roll == "port"
+        total_y_sum += movable_roll_wet_mass * movable_roll_y_center_prt
+
+    total_mass += vessel3_wet_mass
+    total_x_sum += vessel3_wet_mass * vessel3_x_center
+
+    total_mass += electronics_wet_mass
+    total_x_sum += electronics_wet_mass * electronics_x_center
+
+    total_mass += vessel4_wet_mass
+    total_x_sum += vessel4_wet_mass * vessel4_x_center
+
+    total_mass += battery2_wet_mass
+    total_x_sum += battery2_wet_mass * battery2_x_center
+
+    total_mass += movable_pitch_wet_mass
+    total_y_sum += movable_pitch_wet_mass * movable_pitch_y_center
+    total_z_sum += movable_pitch_wet_mass * movable_pitch_z_center
+    if pitch == "forward":
+        total_x_sum += movable_pitch_wet_mass * movable_pitch_x_center_fwd
+    elif pitch == "middle":
+        total_x_sum += movable_pitch_wet_mass * movable_pitch_x_center_mid
+    else:
+        assert pitch == "aft"
+        total_x_sum += movable_pitch_wet_mass * movable_pitch_x_center_aft
+
+    return (total_mass, total_x_sum, total_y_sum, total_z_sum)
+
+
+if False:
+    method1 = center_of_gravity['bladder_half_pitch_middle_roll_starboard']['y'] - \
+        center_of_buoyancy['bladder_half_pitch_middle_roll_starboard']['y']
+    print(method1)
+
+    mass, x_sum, y_sum, z_sum = get_weighted_wet_masses(
+        bladder="half", pitch="middle", roll="starboard")
+    method2 = y_sum / mass
+    print(method2)
+
+battery1_packing_equation = battery1_packing_volume <= pressure_vessel_inner_volume
+battery2_packing_equation = battery2_packing_volume <= pressure_vessel_inner_volume
+
+pitch_minimum_mass, pitch_minimum_x_sum, pitch_minimum_y_sum, pitch_minimum_z_sum = \
+    get_weighted_wet_masses(bladder="empty", pitch="forward", roll="center")
+pitch_minimum_equation1 = pitch_minimum_x_sum / pitch_minimum_z_sum <= math.tan(-60 * pi / 180)
+pitch_minimum_equation2 = pitch_minimum_mass >= 3.0  # sinks to bottom
+
+pitch_maximum_mass, pitch_maximum_x_sum, pitch_maximum_y_sum, pitch_maximum_z_sum = \
+    get_weighted_wet_masses(bladder="full", pitch="aft", roll="center")
+pitch_maximum_equation = pitch_maximum_x_sum / pitch_maximum_z_sum >= math.tan(60 * pi / 180)
+
+pitch_neutral_mass, pitch_neutral_x_sum, pitch_neutral_y_sum, pitch_neutral_z_sum = \
+    get_weighted_wet_masses(bladder="half", pitch="middle", roll="center")
+pitch_neutral_equation1 = pitch_neutral_x_sum / pitch_neutral_z_sum <= math.tan(1.5 * pi / 180)
+pitch_neutral_equation2 = pitch_neutral_x_sum / pitch_neutral_z_sum >= math.tan(-1.5 * pi / 180)
+pitch_neutral_equation3 = pitch_neutral_mass <= -0.1  # floats to surface
+
+roll_minimum_mass, roll_minimum_x_sum, roll_minimum_y_sum, roll_minimum_z_sum = \
+    get_weighted_wet_masses(bladder="half", pitch="middle", roll="starboard")
+roll_minimum_equation = roll_minimum_y_sum / roll_minimum_z_sum <= math.tan(-0 * pi / 180)
+
+constraints = PointFunc({
+    "battery1_packing_equation": battery1_packing_equation,
+    "battery2_packing_equation": battery2_packing_equation,
+    "pitch_minimum_equation1": pitch_minimum_equation1,
+    "pitch_minimum_equation2": pitch_minimum_equation2,
+    "pitch_neutral_equation1": pitch_neutral_equation1,
+    "pitch_neutral_equation2": pitch_neutral_equation2,
+    "pitch_neutral_equation3": pitch_neutral_equation3,
+    "pitch_maximum_equation": pitch_maximum_equation,
+    "roll_minimum_equation": roll_minimum_equation,
+})
+
+print(constraints.input_names)
+print(constraints.output_names)
+
+derived_values = PointFunc({
+    "battery1_packing_volume": battery1_packing_volume,
+    "battery2_packing_volume": battery2_packing_volume,
+    "pitch_minimum_mass": pitch_minimum_mass,
+    "pitch_minimum_angle": atan(pitch_minimum_x_sum / pitch_minimum_z_sum) * 180 / pi,
+    "pitch_neutral_mass": pitch_neutral_mass,
+    "pitch_neutral_angle": atan(pitch_neutral_x_sum / pitch_neutral_z_sum) * 180 / pi,
+    "pitch_maximum_mass": pitch_maximum_mass,
+    "pitch_maximum_angle": atan(pitch_maximum_x_sum / pitch_maximum_z_sum) * 180 / pi,
+    "roll_minimum_mass": roll_minimum_mass,
+    "roll_minimum_angle": atan(roll_minimum_y_sum / roll_minimum_z_sum) * 180 / pi,
+})
+
+
+def print_solutions(points, num=None):
+    points = points.extend(derived_values(points))
+    if num is None:
+        num = points.num_points
+    else:
+        num = min(num, points.num_points)
+    for sol in range(num):
+        print("solution #:", sol)
+        for idx, var in enumerate(points.float_vars):
+            print(var + ":", points.float_data[sol, idx].item())
+        print()
+
+
+bounds = {
+    "battery1_capacity": (5000.0, 15000.0),
+    "battery2_capacity": (5000.0, 15000.0),
+    "foam1_length": (0.0, 2.0),
+    "foam2_length": (0.0, 2.0),
+    "foam3_length": (0.0, 2.0),
+    "movable_pitch_length": (0.10, 1.0),
+    "movable_roll_diameter": (0.05, 0.5),
+}
+
+assert list(bounds.keys()) == list(constraints.input_names)
+
+# generate random points
+points = PointCloud.generate(bounds, 5000)
+
+# minimize errors with newton raphson
+points = points.newton_raphson(constraints, bounds)
+# points.print_info()
+
+# calculate remaining errors
+errors = constraints(points)
+# errors.print_info()
+
+# check constraints
+points = points.prune_by_tolerances(errors, 1e-1)
+print(points.num_points)
+
+for _ in range(10):
+    points.add_mutations([1000, 1000, 0.1, 0.1, 0.1, 0.1, 0.1], 5000)
+    points = points.newton_raphson(constraints, bounds)
+    points = points.prune_by_tolerances(constraints(points), 1e-2)
+    points = points.prune_close_points([500, 500, 0.05, 0.05, 0.05, 0.05, 0.05])
+    print(points.num_points)
+
+print_solutions(points, 10)
